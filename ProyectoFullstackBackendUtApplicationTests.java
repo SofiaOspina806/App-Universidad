6040f550598955b7062988020f7f9908728b2927
SofiaOspina806 <anasofiaos219@gmail.com>
2025-04-25 16:55:19 -0500

    Add files via upload

---
+++ ProyectoFullstackBackendUtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistempaymentut.Proyecto_fullstack_backend._ut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFullstackBackendUtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
